cbc31eeb08cc383178c642853f63d23e96382402
zhousheng <zhousheng@163.com>
2021-10-14 15:51:21 +0800

    0

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <windows.h>
+int main()
+{
+    printf("Hello World\n");
+    system("pause");
+    return 0;
+}
+
+// #include <iostream>
+// using namespace std;
+ 
+// int main()
